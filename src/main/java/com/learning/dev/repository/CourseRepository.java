33cfe2ccf3d6299c96e3494c2b34a89b27d01d9b
Kauan Serafim Galindo <kauanserafimg@gmail.com>
2025-09-26 16:10:40 -0300

    Revert "feat: add repository for Product entity"
    This reverts commit 3f3d9c03202d5c1daf653b833353ffde1d70ebf6.

--- src/main/java/com/learning/dev/repository/CourseRepository.java
+++ src/main/java/com/learning/dev/repository/CourseRepository.java
@@ -1,11 +1,4 @@
 package com.learning.dev.repository;
 
-import com.learning.dev.domain.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    List<Course> findCourseByNameIgnoreCase(String name);
+public interface CourseRepository {
 }
