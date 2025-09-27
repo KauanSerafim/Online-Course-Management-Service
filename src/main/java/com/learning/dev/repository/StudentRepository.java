85e27e194a29b8d2c94e766869ce59dcb379fdfc
Kauan Serafim Galindo <kauanserafimg@gmail.com>
2025-09-27 15:21:50 -0300

    feat: add repository for Student entity

--- src/main/java/com/learning/dev/repository/StudentRepository.java
+++ src/main/java/com/learning/dev/repository/StudentRepository.java
@@ -3,5 +3,9 @@ package com.learning.dev.repository;
 import com.learning.dev.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findStudentByNameIgnoreCase(String name);
 }
