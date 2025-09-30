a343913e110d2b68fd7f722be95dae3377216c94
Kauan Serafim Galindo <kauanserafimg@gmail.com>
2025-09-30 12:42:06 -0300

    feat: add findByNameIgnoreCase method

--- src/main/java/com/learning/dev/repository/CourseRepository.java
+++ src/main/java/com/learning/dev/repository/CourseRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     List<Course> findCourseByNameIgnoreCase(String name);
+
+    Course findByNameIgnoreCase(String name);
 }
